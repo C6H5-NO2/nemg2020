23c0b2c0733169471cc915fe610b78782f65a8ee
MapleTreee <61616230+MapleTreee@users.noreply.github.com>
2020-10-20 17:06:21 +0800

    视角移动缩放、物品点击

---
+++ CameraControler.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+    #region 参数
+    private int cameraMovespeed = 3;
+    private int limitWidth = 20;
+    private int limitHeight = 20;
+    private float zoomMin = 1;
+    private float zoomMax = 20;
+
+    private static Camera mainCamera;
+    #endregion
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+    void Update()
+    {
+        MapControl();
+
+    }
+
+    private void MapControl()
+    {
+        Vector3 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 finalPos;
+
+        if (offset.x > 0.95)
+        {
+            transform.Translate(Vector3.right * cameraMovespeed * Time.deltaTime, Space.World);
+        }
+        if (offset.x < 0.05)
+        {
+            transform.Translate(Vector3.left * cameraMovespeed * Time.deltaTime, Space.World);
+        }
+        if (offset.y > 0.95)
+        {
+            transform.Translate(Vector3.forward * cameraMovespeed * Time.deltaTime, Space.World);
+        }
+        if (offset.y < 0.05)
+        {
+            transform.Translate(Vector3.back * cameraMovespeed * Time.deltaTime, Space.World);
+        }
+        //Limit
+        //限制x，z坐标
+        finalPos = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        if (finalPos.x < -limitWidth)
+        {
+            finalPos.x = -limitWidth;
+        }
+        if (finalPos.x > limitWidth)
+        {
+            finalPos.x = limitWidth;
+        }
+        if (finalPos.z < -limitHeight)
+        {
+            finalPos.z = -limitHeight;
+        }
+        if (finalPos.z > limitHeight)
+        {
+            finalPos.z = limitHeight;
+        }
+        mainCamera.transform.position = finalPos;
+      
+        //Zoom out
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (Camera.main.fieldOfView <= 100)//3d
+                Camera.main.fieldOfView += 2;
+            if (Camera.main.orthographicSize <= zoomMax)//2d
+                Camera.main.orthographicSize += 0.5F;
+        }
+        //Zoom in
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (Camera.main.fieldOfView > 2)//3d
+                Camera.main.fieldOfView -= 2;
+            if (Camera.main.orthographicSize >= zoomMin)//2d
+                Camera.main.orthographicSize -= 0.5F;
+        }
+        
+    }
+}
---
+++ ObjectClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClick : MonoBehaviour
+{
+    void Update()
+    {
+        //点击输出物品信息
+        MousePick();
+    }
+    void MousePick()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //out put here
+                //to UI
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
